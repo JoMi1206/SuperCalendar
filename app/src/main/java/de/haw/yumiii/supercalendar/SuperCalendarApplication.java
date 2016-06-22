26357898aa558ca96aa4435aa1b9f17dc1db4f5c
Johannes Flemke <flemke@in.tum.de>
2016-06-22 22:38:52 +0200

    Only Portraitmode

--- app/src/main/java/de/haw/yumiii/supercalendar/SuperCalendarApplication.java
+++ app/src/main/java/de/haw/yumiii/supercalendar/SuperCalendarApplication.java
@@ -1,6 +1,9 @@
 package de.haw.yumiii.supercalendar;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 
 import com.parse.Parse;
 
@@ -14,6 +17,43 @@ public class SuperCalendarApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity a, Bundle savedInstanceState) {
+                a.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+                
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("p6hd1KznrdoeApttOKGaPmYKn4GIB4KiQWyivdUl")
